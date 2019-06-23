80426ff2ca2767315e6536478f24e971c9c9fce1
W-Venus <wc15236485854@163.com>
2019-06-23 10:04:59 +0800

    点击登录对整个表单进行验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,20 +4,20 @@
       <div class="form-head">
         <img src="./logo_index.png">
       </div>
-       <el-form class="login-form" ref="form" :model="UserForm">
-        <el-form-item>
+       <el-form class="login-form" ref="form" :model="UserForm" :rules="rules">
+        <el-form-item prop="mobile">
           <el-input v-model="UserForm.mobile" placeholder="手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- el-col 栅格布局，一共 24 列，:span 用来指定占用的大小，:offset 用来指定偏移量 -->
           <el-col :span="17">
           <el-input v-model="UserForm.code" placeholder="验证码"></el-input>
           </el-col>
           <el-col :offset="1" :span="6">
-            <el-button @click="handleSendCode">获取验证码</el-button>
+            <el-button id="SendBtn" @click="handleSendCode">获取验证码</el-button>
           </el-col>
         </el-form-item>
-         <el-form-item class="form-argee">
+         <el-form-item class="form-argee" prop="type">
           <el-checkbox-group v-model="UserForm.type">
             <el-checkbox name="type">我已阅读并同意<a href="#">用户协议</a>和<a href="#">隐私条款</a></el-checkbox>
           </el-checkbox-group>
@@ -41,13 +41,48 @@ export default {
       UserForm: {
         mobile: '',
         code: '',
-        type: ''
+        type: true
+      },
+      // 表单验证
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { patten: /\d{11}/, message: '请输入有效的手机号', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { patten: /\d{6}/, message: '请输入有效的验证码', trigger: 'blur' }
+        ]
+        // type: [
+        //   { required: true, message: '请勾选协议' },
+        //   { patten: /true/, message: '请勾选协议' }
+        // ]
       }
     }
   },
 
   methods: {
+    // 发送验证码
     handleSendCode () {
+      // 实现倒计时功能
+      var oBtn = document.getElementById('SendBtn')
+      var flag = true
+      var time = 60
+      oBtn.classList.add('disable')
+      oBtn.innerText = '已发送'
+      if (flag) {
+        flag = false
+        var timer = setInterval(() => {
+          time--
+          oBtn.innerText = time + ' 秒'
+          if (time === 0) {
+            clearInterval(timer)
+            oBtn.innerText = '点击重新获取'
+            oBtn.classList.remove('disable')
+            flag = true
+          }
+        }, 1000)
+      }
       const { mobile } = this.UserForm
       axios({
         method: 'GET',
@@ -68,14 +103,16 @@ export default {
           // 这里可以调用验证实例 captchaObj 的实例方法
           captchaObj.onReady(function () {
             // 验证码ready之后才能调用verify方法显示验证码
-            captchaObj.verify() // 显示验证码
+            captchaObj.verify() // 显示弹框验证码
           }).onSuccess(function () {
             // your code
             // console.log(captchaObj.getValidate())
+            // 验证成功,输出的结果提取转变为短信接口要接收的参数形式
             const {
               geetest_challenge: challenge,
               geetest_seccode: seccode,
               geetest_validate: validate } = captchaObj.getValidate()
+            // 向短信接口发送请求
             axios({
               method: 'GET',
               url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
@@ -85,7 +122,7 @@ export default {
                 validate
               }
             }).then(res => {
-              console.log(res.data)
+              console.log(res.data) // 返回发送短信时的手机号
             })
           }).onError(function () {
             // your code
@@ -94,13 +131,31 @@ export default {
       }
       )
     },
+    // 对整个表单进行验证
     handleLogin () {
+      this.$refs['form'].validate((valid) => {
+        if (!valid) {
+          return
+        }
+        this.submitLogin()
+      })
+    },
+    // 登录
+    submitLogin () {
+      if (this.UserForm.type !== true) {
+        this.$message({
+          showClose: true,
+          message: '警告:请勾选协议',
+          type: 'warning'
+        })
+        return
+      }
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
-        data: this.UserForm
+        data: this.UserForm // 表单同步的手机号和验证码
       })
-        .then(res => {
+        .then(res => { // 状态码>=200 <400时,进入这里,表示登录成功
           // console.log(res.data)
           // 登录成功,提示消息
           this.$message({
@@ -112,7 +167,7 @@ export default {
             name: 'home'
           })
         })
-        .catch((e) => {
+        .catch((e) => { // 状态码>=400时进入这里,表示登录失败
         // 登录失败,提示消息
           this.$message.error('登录失败,请检查手机号或验证码')
         })
