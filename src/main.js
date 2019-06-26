8455dc4c155180fdcc143a836841e4538750f3ed
W-Venus <wc15236485854@163.com>
2019-06-26 17:45:12 +0800

    使用请求拦截器统一配置token

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 // 引入进度条样式
 import 'nprogress/nprogress.css'
+import { getUser } from '@/utils/auth'
 
 Vue.config.productionTip = false
 // 将axios配置到vue原型中,这样在组件中可以直接使用,不用加载
@@ -16,6 +17,25 @@ Vue.prototype.$http = axios
 // 配置axios接口路径
 // axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 axios.defaults.baseURL = 'http://toutiao.course.itcast.cn/mp/v1_0'
+
+// 配置请求拦截器,统一处理token,方便后续使用
+axios.interceptors.request.use(config => {
+  const user = getUser()
+  // 判断 是否有user用户信息,有的话就设置
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config // 允许请求发送的开关
+}, error => {
+  return Promise.reject(error)
+})
+
+// 配置响应拦截器,统一处理响应的结果
+// axios.interceptors.response.use(function (response) {
+//   return response
+// }, function (error) {
+//   return Promise.reject(error)
+// })
 
 Vue.use(ElementUI)
 
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -14,8 +14,11 @@
       </div>
     <!-- 列表部分 -->
       <el-table class="table-card" :data="tableData" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="date" label="封面" width="180"></el-table-column>
+        <el-table-column prop="name" label="标题" width="180"></el-table-column>
+        <el-table-column prop="name" label="状态" width="180"></el-table-column>
+        <el-table-column prop="name" label="发布时间" width="180"></el-table-column>
+        <el-table-column prop="name" label="操作" width="180"></el-table-column>
         <el-table-column prop="address" label="地址"></el-table-column>
     </el-table>
     <!-- /列表部分 -->
@@ -32,27 +35,24 @@ export default {
 
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      tableData: []
     }
   },
-
-  methods: {}
+  created () {
+    // 初始化开始就请求数据
+    this.loadArticles()
+  },
+  methods: {
+    async loadArticles () {
+      // 获取token,设置请求头,请求头中设置 Authorization
+      const res = await this.$http({
+        method: 'GET',
+        url: '/articles'
+      })
+      this.tableData = res.data.data.results
+      console.log(res.data.data.results)
+    }
+  }
 }
 </script>
 
