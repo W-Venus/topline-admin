1e32b75b06d39cedf807335a650aba7ca00f8e8c
W-Venus <wc15236485854@163.com>
2019-06-29 20:44:13 +0800

    创建素材管理页面&配置路由&布局完成

---
+++ src/views/media/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-card class="media-card">
+    <div slot='header'>
+      <span>素材管理</span>
+    </div>
+    <div class="action">
+      <el-radio-group v-model="active">
+        <el-radio-button label="全部"></el-radio-button>
+        <el-radio-button label="收藏"></el-radio-button>
+      </el-radio-group>
+      <el-button type="primary">上传图片</el-button>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'media',
+
+  data () {
+    return {
+      active: '全部'
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+  .action {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 20px;
+  }
+</style>
