47330a8df88bca0ade3f33b740c86f0c8fe2a235
W-Venus <wc15236485854@163.com>
2019-06-29 18:01:32 +0800

    加载评论列表

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -3,16 +3,22 @@
     <div slot="header">
       <span>评论管理</span>
     </div>
-    <el-table :data="articles">
+    <!-- 评论列表 -->
+    <el-table :data="comments">
       <el-table-column label="标题" prop="title"></el-table-column>
       <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
       <el-table-column label="评论粉丝数" prop="fans_comment_count"></el-table-column>
       <el-table-column label="允许评论">
-        <template>
-          123
-        </template>
+        <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
       </el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      class="comment-page"
+      background
+      layout="prev, pager, next"
+      :total="1000">
+    </el-pagination>
   </el-card>
 </template>
 
@@ -22,12 +28,37 @@ export default {
 
   data () {
     return {
+      comments: [],
+      value: true
     }
   },
-
-  methods: {}
+  created () {
+    this.loadComments()
+  },
+  methods: {
+    // 请求评论列表
+    async loadComments () {
+      try {
+        const data = await this.$http({
+          method: 'GET',
+          url: '/articles',
+          params: {
+            response_type: 'comment'
+          }
+        })
+        // console.log(data.results)
+        this.comments = data.results
+      } catch (err) {
+        this.$message.error('请求评论列表失败')
+      }
+    }
+  }
 }
 </script>
 
 <style lang='less' scoped>
+  .comment-page {
+     margin-top: 20px;
+     text-align: center;
+ }
 </style>
