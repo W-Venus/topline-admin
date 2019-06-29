0cb9b7513affb877bf6000d1ccfec72511e3cae3
W-Venus <wc15236485854@163.com>
2019-06-29 20:24:55 +0800

    评论页面评论状态开关的处理

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -9,7 +9,16 @@
       <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
       <el-table-column label="评论粉丝数" prop="fans_comment_count"></el-table-column>
       <el-table-column label="允许评论">
-        <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        <template slot-scope="scope">
+          <!-- disabled 控制开关的禁用状态 -->
+          <el-switch
+          :disabled="scope.row.disabled"
+          v-model="scope.row.comment_status"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+          @change="handleStatus(scope.row)"
+          ></el-switch>
+        </template>
       </el-table-column>
     </el-table>
     <!-- 分页 -->
@@ -28,8 +37,7 @@ export default {
 
   data () {
     return {
-      comments: [],
-      value: true
+      comments: []
     }
   },
   created () {
@@ -46,10 +54,40 @@ export default {
             response_type: 'comment'
           }
         })
+        // 手动添加一个disabled属性,用来控制开关的禁用状态
+        data.results.forEach(item => {
+          item.disabled = false
+        })
         // console.log(data.results)
         this.comments = data.results
       } catch (err) {
         this.$message.error('请求评论列表失败')
+      }
+    },
+    // 修改评论状态
+    async handleStatus (item) {
+      // console.log(item)
+      try {
+        // 发请求的时候禁用开关
+        item.disabled = true
+        await this.$http({
+          method: 'PUT',
+          url: '/comments/status',
+          params: {
+            article_id: item.id.toString() // 将id转为字符串
+          },
+          data: {
+            allow_comment: item.comment_status // 当前开关的状态
+          }
+        })
+        // 请求结束的时候开启开关
+        item.disabled = false
+        this.$message({
+          type: 'success',
+          message: `${item.comment_status ? '启用' : '禁用'}按钮成功`
+        })
+      } catch (err) {
+        this.$message.error('操作失败')
       }
     }
   }
