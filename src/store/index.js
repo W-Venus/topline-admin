05dc60aad02c4bf3dea3609cdc9825bf94c461f8
W-Venus <wc15236485854@163.com>
2019-06-30 21:11:58 +0800

    man.js中引入store并使用其管理个人信息&与头部的用户信息保持同步更新

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { getUser } from '@/utils/auth'
+import { getUser, setUser } from '@/utils/auth'
 
 Vue.use(Vuex)
 
@@ -11,8 +11,18 @@ const store = new Vuex.Store({
   state: {
     user: getUser() || {} // user 的初始数据来自于本地存储
   },
+  // 类似组件的methods,定义mutation来修改state状态,在组件中调用mutation函数完成对状态的修改
+  // 在组件中调用方法是 this.$store.commit('函数名')
   mutations: {
-
+    // mutations 函数默认接收一个参数,就是state
+    // 在这里可以通过 state.xxx = xxx 完成对容器状态的修改
+    changeUser (state, user) {
+      // 接收修改后的用户信息,在这里对state中的数据进行修改
+      // 将后者混入到前者里面,后者会覆盖前者
+      Object.assign(state.user, user)
+      // 将修改后的数据重新放入本地存储中,更新一下本地存储的信息
+      setUser(state.user)
+    }
   }
 })
 
